a000afbe81e33b5b869ed5cb675f7860204a5930
Emine Ugurlu <emineugurlu957@gmail.com>
2025-12-09 20:24:35 +0300

    Mandelbrot

--- mandelbrot_cizici.py
+++ mandelbrot_cizici.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# --- 1. FONKSİYON VE AYARLAR ---
-MAX_ITERASYON = 100 
+
+MAX_ITERASYON = 500
 
 def mandelbrot_hesapla(c):
     z = 0 + 0j
@@ -12,39 +12,36 @@ def mandelbrot_hesapla(c):
             return n 
     return MAX_ITERASYON
 
-# --- 2. RESİM VE DÜZLEM AYARLARI ---
+
 GENISLIK = 800
 YUKSEKLIK = 600
 
-REEL_MIN = -2.0
-REEL_MAX = 1.0
-SANAL_MIN = -1.0
-SANAL_MAX = 1.0
+REEL_MIN = -0.8
+REEL_MAX = -0.7
+SANAL_MIN = 0.1
+SANAL_MAX = 0.2
 
-# Dizi oluşturma
 resim_dizisi = np.zeros((YUKSEKLIK, GENISLIK))
 
-# --- 3. HESAPLAMA DÖNGÜSÜ ---
+
 for x in range(GENISLIK):
     for y in range(YUKSEKLIK):
         
-        # Koordinat Eşleme
+        
         reel_kisim = REEL_MIN + (x / GENISLIK) * (REEL_MAX - REEL_MIN)
         sanal_kisim = SANAL_MIN + (y / YUKSEKLIK) * (SANAL_MAX - SANAL_MIN)
         c = complex(reel_kisim, sanal_kisim)
         
-        # Hesaplama
+        
         iterasyon_sayisi = mandelbrot_hesapla(c)
         
-        # Diziye Kaydetme
+        
         resim_dizisi[y, x] = iterasyon_sayisi
 
-# --- 4. GÖRSELLEŞTİRME KISMI (Matplotlib) ---
-# cmap='hot' yerine farklı bir renk haritası kullanacağız.
+
 plt.imshow(resim_dizisi, cmap='twilight') 
 plt.axis('off')
 plt.savefig("mandelbrot_ilk_cizim.png", bbox_inches='tight', pad_inches=0)
 
 print("Mandelbrot kümesi çizildi: mandelbrot_ilk_cizim.png")
 
-# plt.show()
